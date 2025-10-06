f37a1967021e1d3380986dc42aae7ed236679eed
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-10-06 11:12:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,170 @@
+// variables
+
+
+// var - global scope
+
+// let - block scope
+
+// const - block scope
+
+
+// var
+
+
+var a = 10   // declration // intiazialiation
+
+a = 12       // reuse      // reinti
+
+var a = 30   // redeclration
+
+
+console.log(a)
+
+
+// let
+
+let b = 5  // declration   // inti
+
+b = 10     // reuse        // reint
+
+// let b   // not accept let redeclration
+
+console.log(b);
+
+
+// const
+
+const c = 5  // declration // inti
+
+// c = 10    // reuse   // reinti not accept
+
+// const c = 10  // redeclration not accept
+
+console.log(c);
+
+
+
+let apple = "apple"
+
+apple = "ooty apple"
+
+apple = "goa apple"
+
+console.log(apple);
+
+
+
+var count = 10
+
+count = 10+1
+
+count = count + count
+
+let b1 = count
+
+
+console.log(b1);
+
+
+// printing statement
+
+// 1. console.log()
+
+console.log(100);
+
+let b2 = 12
+
+console.log(b2);
+
+
+// 2. alert()
+
+// alert("hello every one these are my first class in js")
+
+// 3. confirm()
+
+// confirm("are you want juice ?")
+
+// 4. prompt()
+
+// let name1 = prompt("what is your name :")
+
+// console.log(name1);
+
+
+// 5. document.writeLn()
+
+// document.writeln("hello")
+
+
+// "hi there" - alert
+
+
+// let a1 = confirm("hello ?")
+
+// console.log(a1);
+
+
+
+
+// console method
+
+// log
+
+console.log(1000);
+
+
+// error
+
+console.error(1000)
+
+// warn
+
+console.warn(1000);
+
+
+// clear
+
+console.clear()
+
+
+let a3 = "kevin"
+
+let a4 = 123
+
+let a5 = "123"
+
+
+// Data types
+
+// 1. primitive datatypes
+
+// 1. string 
+
+console.log("string : ","hello",123,"123");
+
+// 2. number
+
+console.log("number : ",123);
+
+
+// 3. boolean
+
+console.log("boolean : ", true , false);
+
+
+// 4. undefined
+
+let a6
+
+console.log(a6);
+
+// 5. null
+
+let a7 = prompt()
+
+console.log(a7);
+
+
+
+// 2. non primitive datatypes
